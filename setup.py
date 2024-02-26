c5ec67cb6c18ff4065c18a6579b60cdc5198f3e2
Rajesh Khangar <rkhangar1998@gmail.com>
2024-02-26 08:46:57 +0530

    add cockiecutter  temp

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Real estate proj using mlops priciple',
+    author='Rajesh Khangar',
+    license='',
+)
