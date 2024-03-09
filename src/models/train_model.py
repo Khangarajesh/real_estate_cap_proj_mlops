9f1fdd266c6b84138c64d42b5c79b930fb029bff
Rajesh Khangar <rkhangar1998@gmail.com>
2024-03-10 02:11:18 +0530

    Save model function added on train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import pathlib
-
+import joblib
 
 
 def find_the_best_model(X_train,y_train,X_test,y_test):
@@ -112,21 +112,34 @@ def find_the_best_model(X_train,y_train,X_test,y_test):
         
     return model
 
+def save_model(model,output_path):
+    joblib.dump(model,output_path,'/model.joblib')
 
 
-if __name__ == '__main__':
-    
+def main():
     curr_dir = pathlib.Path(__file__)
     home_dir = curr_dir.parent.parent.parent
     file_path = home_dir.as_posix() + '/data/processed/gurgaon_properties_post_feature_selection.csv'
+    
     df = pd.read_csv(file_path)
     x = df.drop('price', axis = 1)
     y = df['price']
     X_train,X_test,y_train,y_test = train_test_split(x,y, test_size = 0.2, random_state = 42)
-    find_the_best_model(X_train,y_train,X_test,y_test)
     
+    model = find_the_best_model(X_train,y_train,X_test,y_test)     
+    output_path = home_dir.as_posix() + '/model'
+    
+    save_model(model,output_path)
+
+if __name__ == '__main__':
+    
+    main()
+    
+    
+
     
     
     #save model code pending
     #use params.yaml file pending
     #solve the error ehile dvc repro
+    #check the file 
